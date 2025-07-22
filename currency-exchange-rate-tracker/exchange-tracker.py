d1ed02e2beaf9abd01c9123dbaf93c5c6b4e090a
averagejoesec <gonzalezjlusmc@gmail.com>
2025-07-22 20:34:24 +0200

    converted one currency

--- currency-exchange-rate-tracker/exchange-tracker.py
+++ currency-exchange-rate-tracker/exchange-tracker.py
@@ -1,5 +1,6 @@
 import requests as r
 import datetime
+import json
 
 
 
@@ -9,12 +10,18 @@ currency = input('Enter the base currency (e.g., USD): ')
 url = r.get(f'https://open.er-api.com/v6/latest/{currency}')
 response = url.json()
 
+with open('currency-exchange-rate-tracker/output.txt', 'w') as file:
+    file.write(json.dumps(response, indent=2))
+    file.close
+
 print('Available currencies: USD, EUR, GBP')
 target_currency = input('Enter target currencies (comma-seprated, e.g., EUR, GBP): ')
-amount = int(input(f'Enter the amount in {currency}'))
+amount = float(input(f'Enter the amount to exchange in {currency}: '))
 
-# def convert_currency(currency, target_currency, amount):
-#     print('Conversion Results:\n ')
+def convert_currency(currency, target_currency, amount):
+    target_currency_amount = response['rates'][target_currency]
+    converted_rate = float(target_currency_amount) * amount
+    print(f'Conversion Results:\n{round(converted_rate, 2)}')
 
 
-# convert_currency()
+convert_currency(currency, target_currency, amount)
