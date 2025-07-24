74e092819ef6335c5ba209ecfe3e9848e5913afe
averagejoesec <gonzalezjlusmc@gmail.com>
2025-07-24 21:46:11 +0200

    complete currency api project

--- currency-exchange-rate-tracker/exchange-tracker.py
+++ currency-exchange-rate-tracker/exchange-tracker.py
@@ -13,21 +13,25 @@ with open('currency-exchange-rate-tracker/output.txt', 'w') as file:
     file.close
 
 print('Available currencies: USD, EUR, GBP')
-target_currency = input('Enter target currencies (comma-seprated, e.g., EUR, GBP): ')
+
+target_currencies = input('Enter target currencies (comma-seprated, e.g., EUR, GBP): ')
+target_currencies = target_currencies.split(", ")
+
 amount = float(input(f'Enter the amount to exchange in {currency}: '))
 
-def convert_currency(currency, target_currency, amount):
-    target_currency_amount = response['rates'][target_currency]
-    converted_rate = float(target_currency_amount) * amount
-    print(f'Conversion Results:\n{amount} {currency} = {round(converted_rate, 2)} {target_currency}')
-    
-    conversion_results = f"Base currency: {currency}\nAmount: {amount} {currency}\n{amount} {currency} = {round(converted_rate, 2)} {target_currency}\nTimestamp: {datetime.now()}"
+def convert_currency(currency, target_currencies, amount):
+    for target_currency in target_currencies:
+        target_currency_amount = response['rates'][target_currency]
+        converted_rate = float(target_currency_amount) * amount
+        print(f'Conversion Results for {target_currency}:\n{amount} {currency} = {round(converted_rate, 2)} {target_currency}')
+        
+        conversion_results = f"Base currency: {currency}\nAmount: {amount} {currency}\n{amount} {currency} = {round(converted_rate, 2)} {target_currency}\nTimestamp: {datetime.now()}\n\n"
 
-    filename = 'currency-exchange-rate-tracker/conversion_results.txt'                   
-    with open(filename, 'w') as file:
-        file.write(conversion_results)
-        file.close()
+        filename = 'currency-exchange-rate-tracker/conversion_results.txt'                   
+        with open(filename, 'a') as file:
+            file.write(conversion_results)
+            file.close()
     print(f'Results saved to {filename}.')
 
 
-convert_currency(currency, target_currency, amount)
+convert_currency(currency, target_currencies, amount)
