5670eba31554904ae1b220b76ac80dc7632580ed
averagejoesec <gonzalezjlusmc@gmail.com>
2025-07-29 12:43:22 +0200

    add streamlit plot

--- basic_scripts_practice/scrape_temp_data/main.py
+++ basic_scripts_practice/scrape_temp_data/main.py
@@ -3,8 +3,8 @@ import selectorlib
 from datetime import datetime
 
 URL = "https://programmer100.pythonanywhere.com/"
-OUTPUT_FILE = "basic_scripts_practice/scrape_temp_data/output.txt"
-NOW = datetime.now()
+OUTPUT_FILE = "output.txt"
+NOW = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
 
 def scrape(url):
     response = rq.get(url)
@@ -13,14 +13,14 @@ def scrape(url):
 
 
 def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("basic_scripts_practice/scrape_temp_data/extract.yaml")
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["temp"]
     return value
 
 
 def save_temp(temp):
     with open(OUTPUT_FILE, 'a') as file:
-        file.write(f"{NOW}, {temp}\n")
+        file.write(f"{NOW},{temp}\n")
 
 if __name__ == "__main__":
     scraped_scource = scrape(URL)
