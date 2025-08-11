095bbdcdaf2b986aac51a37fc0c611463ef74a6a
averagejoesec <gonzalezjlusmc@gmail.com>
2025-08-11 18:15:25 +0200

    complete first part of time format

--- basic_scripts_practice/smart_clock_formatter/main.py
+++ basic_scripts_practice/smart_clock_formatter/main.py
@@ -2,11 +2,13 @@ from datetime import datetime
 
 def verbose_time(time_format):
     present_time = datetime.now()
+    time_frame = ''
     if time_format == 12:
         hour_clock = '%I'
+        time_frame = '%p'
     else:
         hour_clock = '%H'
-    verbose_date = datetime.strftime(present_time, f'%A, %B %d, %Y - {hour_clock}:%M')
+    verbose_date = datetime.strftime(present_time, f'%A, %B %d, %Y - {hour_clock}:%M {time_frame}')
     print(f"Current Local Time (Verbose): {verbose_date}")
 
 
