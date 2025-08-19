14c3055fa0582352ab8ed6b513d837b5f0e40191
averagejoesec <gonzalezjlusmc@gmail.com>
2025-08-19 08:58:11 +0200

    fix errors begin csv output

--- basic_scripts_practice/grade_calculator/main.py
+++ basic_scripts_practice/grade_calculator/main.py
@@ -1,37 +1,49 @@
+import csv
+
 def average(scores):
     scores_list = scores.split(", ")
     scores_length = float(len(scores_list))
     total = 0
     for score in scores_list:
-        score = round(float(score), 2)
-        total = score + total
+        try:
+            score = round(float(score), 2)
+            if score >= 0 and score <= 100:
+                total = score + total
+            else:
+                print("Invalid input: all scores must be numbers between 0 and 100 and separated by commas")
+                break
+        except:
+            print("Invalid input: all scores must be numbers between 0 and 100 and separated by commas")
+            exit()
     average_score = total / scores_length
     average_score = round(average_score, 2)
 
     grade = ""
-    if average_score >= 90:
+    if average_score >= 90.00:
         grade = "A"
     elif average_score >= 80 and average_score <= 89:
         grade = "B"
-    elif average_score >= 79 and average_score <= 70:
+    elif average_score >= 70 and average_score <= 79:
         grade = "C"
     elif average_score >= 60 and average_score <= 69:
         grade = "D"
-    elif average_score >= 80 and average_score <= 89:
+    elif average_score <= 59:
         grade = "F"
     
     verdict = "Fail"
     if grade == "A" or "B" or "C" or "D":
         verdict = "Pass"
-
     return average_score, grade, verdict
 
+
+def write_to_csv:
+    with open("grades.csv", "w", newline="") as csvfile:
+        fieldnames = ["Score", "Grade", "Pass/Fail"]
 
 if __name__ == "__main__":
     scores = input("Enter scores separated by commas: ")
     average_score, grade, verdict = average(scores)
-    print(f"""
-            Average: {average_score}
-            Letter Grade: {grade}
-            Result: {verdict}
-            """)
+    if average_score and grade and verdict:
+        print(f"Average: {average_score}")
+        print(f"Letter Grade: {grade}")
+        print(f"Result: {verdict}")
