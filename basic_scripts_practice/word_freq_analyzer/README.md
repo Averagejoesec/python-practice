b544d18492292e491610cb7028c9ff7561c0ea81
averagejoesec <gonzalezjlusmc@gmail.com>
2025-09-18 18:46:20 +0200

    word analysis

---
+++ basic_scripts_practice/word_freq_analyzer/README.md
@@ -0,0 +1,24 @@
+## Project Task
+# The program should:
+
+Accept text input from the user (or read from a file)
+
+Clean the text by removing punctuation and converting to lowercase
+
+Count the frequency of each word
+
+Display the most common words in a formatted report
+
+Show basic text statistics (total words, unique words, etc.)
+
+# Bonus (optional):
+
+Filter out common stop words (the, and, of, etc.)
+
+Create a simple bar chart of word frequencies
+
+Save results to a file
+
+Find the longest and shortest words
+
+This project gives you hands-on practice with dictionaries, string processing, and data analysis — essential skills for text mining, content analysis, and natural language processing.
---
+++ basic_scripts_practice/word_freq_analyzer/main.py
@@ -0,0 +1,43 @@
+# Accept text input from the user (or read from a file)
+user_input = input("Enter your text: ").lower()
+
+# Clean the text by removing punctuation and converting to lowercase
+for punctuation in ',.?;"-':
+    user_input = user_input.replace(punctuation, "")
+    cleaned_text = user_input.split(" ")
+
+total_words = len(cleaned_text)
+
+sum = 0
+for w in cleaned_text:
+    word_length = len(w)
+    sum += int(word_length)
+
+average_word_length = sum / total_words
+
+# Count the frequency of each word
+for w in cleaned_text:
+    frequency = {i:cleaned_text.count(i) for i in cleaned_text}
+    frequency = {k: v for k, v in sorted(frequency.items(), key=lambda item: item[1])}
+
+print(frequency)
+unique_words = len(frequency)
+
+# Display the most common words in a formatted report
+print(f"""Top 5 Most Common Words:
+      1. {list(frequency)[-1]}: {frequency[list(frequency)[-1]]} times
+      2. {list(frequency)[-2]}: {frequency[list(frequency)[-2]]} times
+      3. {list(frequency)[-3]}: {frequency[list(frequency)[-3]]} times
+      4. {list(frequency)[-4]}: {frequency[list(frequency)[-4]]} times
+      5. {list(frequency)[-5]}: {frequency[list(frequency)[-5]]} times
+
+      Longest word: {list(frequency)[-1]} ({frequency[list(frequency)[-1]]} letters)
+      Shortest word: {list(frequency)[0]} ({frequency[list(frequency)[0]]} letter(s))
+""")
+
+# Show basic text statistics (total words, unique words, etc.)
+print(f"""=== Word Frequency Analysis ===
+Total words: {total_words}
+Unique words: {unique_words}
+Average word length: {average_word_length}
+            """)
