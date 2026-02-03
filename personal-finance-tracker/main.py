a97d565619eb4fca5b34a125fc9475674c79a16e
averagejoesec <gonzalezjlusmc@gmail.com>
2026-02-03 22:00:39 +0100

    add expense fn

--- personal-finance-tracker/main.py
+++ personal-finance-tracker/main.py
@@ -5,20 +5,40 @@ expenses = []
 
 # Allows users to add expenses with amount, category, and description
 def add_expenses():
-    print("Hello from personal-finance-tracker!")
+    print("Add New Expense")
+    amount = float(input("Amount: "))
+    category = str(input("Category: "))
+    description = str(input("Description: "))
+
+    expenses.append({"Amount" : amount, "Category": category, "Description": description})
+    print(expenses)
 
 # Displays all expenses in a clean, formatted output
-def show_expenses():
+def view_expenses():
     # Shows spending breakdown by category
     pass
 
 # Calculates and shows total spending
-def calculate_total():
+def view_summary():
     pass
 
 # Uses a simple menu system for user interaction
 def main_menu():
-    pass
+    print("""Menu:
+1. Add Expense
+2. View All Expenses
+3. View Summary
+4. Exit
+""")
+    userOption = int(input("Choose option: "))
+    
+    match userOption:
+        case 1:
+            add_expenses()
+        case 2:
+            view_expenses()
+        case 3:
+            view_summary()
 
 
 main_menu()
