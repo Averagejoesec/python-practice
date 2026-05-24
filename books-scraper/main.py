51db02974e7bfda2f32994a081e7d00bc13c467e
averagejoesec <gonzalezjlusmc@gmail.com>
2026-05-24 14:07:11 +0200

    add euros converter function

--- books-scraper/main.py
+++ books-scraper/main.py
@@ -7,15 +7,24 @@ response = requests.get(URL)
 soup = BeautifulSoup(response.content, "html.parser")
 books = soup.find_all("article", class_="product_pod")
 
+def convert_to_euros(price):
+    pounds = float(price.replace("£", ""))
+    euros = round(pounds * 1.16, 2)
+    price_in_euros = "€" + str(euros)
+    return price_in_euros
+
+
 def get_books():
     list_of_books = []
     for article in books:
         title = article.h3.a["title"]
         price = article.find("p", class_="price_color").text.strip()
-        book = (title, price)
+        converted_price = convert_to_euros(price)
+        book = (title, converted_price)
         list_of_books.append(book)
     print(list_of_books)
     return list_of_books
+
 
 def write_to_db(list_of_books):
     conn = sqlite3.connect("books.db")
@@ -24,6 +33,7 @@ def write_to_db(list_of_books):
     c.executemany("INSERT INTO books (title, price) VALUES (?, ?)", list_of_books)
     conn.commit()
     conn.close()
+
 
 if __name__ == "__main__":
     get_books()
