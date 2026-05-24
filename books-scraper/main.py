6f36d39b0ef9d11729c5ce14f203c3034d102d02
averagejoesec <gonzalezjlusmc@gmail.com>
2026-05-24 13:58:46 +0200

    convert to functions

--- books-scraper/main.py
+++ books-scraper/main.py
@@ -2,27 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-# ✅ Fetches data from the website
 URL = "http://books.toscrape.com/"
 response = requests.get(URL)
 soup = BeautifulSoup(response.content, "html.parser")
 books = soup.find_all("article", class_="product_pod")
 
-# ✅ Extracts each book’s title and price from the main page.
-list_of_books = []
-for article in books:
-    title = article.h3.a["title"]
-    price = article.find("p", class_="price_color").text.strip()
-    book = (title, price)
-    list_of_books.append(book)
+def get_books():
+    list_of_books = []
+    for article in books:
+        title = article.h3.a["title"]
+        price = article.find("p", class_="price_color").text.strip()
+        book = (title, price)
+        list_of_books.append(book)
+    print(list_of_books)
+    return list_of_books
 
-# ✅ Saves this data into a local SQLite database file called books.db, inside a table named books with columns title and price.
-conn = sqlite3.connect("books.db")
-c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, price TEXT)")
-c.executemany("INSERT INTO books (title, price) VALUES (?, ?)", list_of_books)
-conn.commit()
-conn.close()
+def write_to_db(list_of_books):
+    conn = sqlite3.connect("books.db")
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, price TEXT)")
+    c.executemany("INSERT INTO books (title, price) VALUES (?, ?)", list_of_books)
+    conn.commit()
+    conn.close()
 
-# ✅ Prints out the data it found so you can see it before it goes into the database.
-print(list_of_books)
+if __name__ == "__main__":
+    get_books()
